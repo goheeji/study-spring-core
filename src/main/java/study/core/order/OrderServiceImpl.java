651caaed35c1e91ad12e340f140683befef626c7
goheeji <gmlwl0489@daum.net>
2021-06-16 22:51:22 +0900

    Refactor: 인터페이스에만 의존하도록 의존관계 변경(OCP 및 DIP 만족)

--- src/main/java/study/core/order/OrderServiceImpl.java
+++ src/main/java/study/core/order/OrderServiceImpl.java
@@ -1,14 +1,18 @@
 package study.core.order;
 
 import study.core.discount.DiscountPolicy;
-import study.core.discount.FixDiscountPolicy;
 import study.core.member.Member;
 import study.core.member.MemberRepository;
 import study.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService{
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+//    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+//    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+
+    //OCP,DIP를 위반하므로 아래와같이 설계 변경
+    private DiscountPolicy discountPolicy;
+
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         Member member = memberRepository.findById(memberId);
