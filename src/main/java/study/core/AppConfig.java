15ba36fe6417ac5503f1a8419279a02f61b7b689
goheeji <gmlwl0489@daum.net>
2021-06-16 23:57:26 +0900

    Refactor: 역할에 따른 구현체 생성부분 메서드 분리

--- src/main/java/study/core/AppConfig.java
+++ src/main/java/study/core/AppConfig.java
@@ -3,6 +3,7 @@ package study.core;
 import study.core.discount.DiscountPolicy;
 import study.core.discount.FixDiscountPolicy;
 import study.core.discount.RateDiscountPolicy;
+import study.core.member.MemberRepository;
 import study.core.member.MemberService;
 import study.core.member.MemberServiceImpl;
 import study.core.member.MemoryMemberRepository;
@@ -13,9 +14,18 @@ public class AppConfig {
 
     //생성자 주입
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
     public OrderService orderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository(), new RateDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy(){
+        return new FixDiscountPolicy();
     }
 }
